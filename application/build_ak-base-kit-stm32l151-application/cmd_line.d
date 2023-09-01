819eff592f0defda133f48e2be014ee7d4385778
VietDung2k1 <truongdung755@gmail.com>
2023-09-01 15:38:55 +0700

    Quockhhanh2/9_Linux

---
+++ application/build_ak-base-kit-stm32l151-application/cmd_line.d
@@ -0,0 +1,24 @@
+build_ak-base-kit-stm32l151-application/cmd_line.o: \
+ sources/common/cmd_line.c \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/string.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/string.h \
+ sources/common/cmd_line.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h \
+ sources/common/xprintf.h
---
+++ application/build_ak-base-kit-stm32l151-application/itoa.d
@@ -0,0 +1,19 @@
+build_ak-base-kit-stm32l151-application/itoa.o: \
+ sources/platform/stm32l/arduino/cores/itoa.cpp \
+ sources/platform/stm32l/arduino/cores/itoa.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/string.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h \
+ /home/vietdung/workspace/tools/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/string.h
